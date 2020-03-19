7713d5bf5f4bb61c30869fb81a0c8e090407a607
OA Wu <comdan66@gmail.com>
2020-03-19 12:41:47 +0800

    Fix layout

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,20 @@
  * @license     http://opensource.org/licenses/MIT  MIT License
  * @link        https://www.ioa.tw/
  */
+
+Load.css('icon.css')
+Load.css('index.css')
+
+Load.func({
+  data: {
+  },
+  mounted () {
+    Load.closeLoading()
+  },
+  template: El(`
+    main
+      label.card => title=目前濕度   unit=%   *text='a'
+      label.card => title=目前溫度   unit=°C   *text='a'
+  `)
+})
+
