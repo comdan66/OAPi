5c31f756f674ef6771c0c8a50c49fcb2d54296eb
OA Wu <comdan66@gmail.com>
2020-03-24 15:40:13 +0800

    tmp

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,8 @@
  * @link        https://www.ioa.tw/
  */
 
-window.Load.css('gauge.css')
-window.Load.css('index.css')
+window.Load.css('https://comdan66.github.io/OAPi/gauge.css')
+window.Load.css('https://comdan66.github.io/OAPi/index.css')
 
 let serverData = null
 
